ba860514a88a5d930439d428357ed932ca2e270d
erikbenton <erik.benton@hotmail.com>
2017-12-08 15:36:06 -0500

    init commit, laying down framework

---
+++ LinkedList.rb
@@ -0,0 +1,56 @@
+class LinkedList
+
+	def initialize
+		
+	end
+
+	def append(val)
+		
+	end
+
+	def prepend(val)
+		
+	end
+
+	def size
+		
+	end
+
+	def head
+		
+	end
+
+	def tail
+		
+	end
+
+	def at(index)
+		
+	end
+
+	def pop
+		
+	end
+
+	def contains?(val)
+		
+	end
+
+	def find(data)
+		
+	end
+
+	def to_s
+		
+	end
+
+	def insert_at(index)
+		
+	end
+
+	def remove_at(index)
+		
+	end
+
+
+end
